7b3bf6000cb0640ec77fa5b4bb884b5c6663102d
Daim-Nickel-Penny <1804449@kiit.ac.in>
2022-11-19 15:50:35 +0530

    Added router and morgan

---
+++ src/router.ts
@@ -0,0 +1,37 @@
+import { Router } from "express";
+
+const router = Router();
+/**
+ * Product
+ */
+
+router.get("/product", (req, res) => {
+  res.json({
+    msg: "hello",
+  });
+});
+router.get("/product/:id", () => {});
+router.put("/product/:id", () => {});
+router.post("/product", () => {});
+router.delete("/product/:id", () => {});
+
+/**
+ * Update
+ */
+router.get("/update", () => {});
+router.get("/update/:id", () => {});
+router.put("/update/:id", () => {});
+router.post("/update", () => {});
+router.delete("/update/:id", () => {});
+
+/**
+ * Update Points
+ */
+
+router.get("/updatepoint", () => {});
+router.get("/updatepoint/:id", () => {});
+router.put("/updatepoint/:id", () => {});
+router.post("/updatepoint", () => {});
+router.delete("/updatepoint/:id", () => {});
+
+export default router;
--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,16 @@
 import express from "express";
-
+import router from "./router";
+import morgan from "morgan";
 const app = express();
+
+app.use(morgan("dev"));
 
 app.get("/", (req, res) => {
   console.log("hello");
   res.status(200);
   res.json({ message: "hello" });
 });
+
+app.use("/api", router);
 
 export default app;
